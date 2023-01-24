32217f7cb4f85f863808041a17450d09419f762d
Sodre <mateus.sodre.s@gmail.com>
2023-01-24 17:03:27 -0300

    correcao de um bug

--- src/TipoVariaveis.java
+++ src/TipoVariaveis.java
@@ -1,4 +1,4 @@
-public class App {
+public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500.44;
         System.out.println(salarioMinimo);
